a9f5e43263bc9e0d9df505d8e5b927294897fb66
DrevilYT <68735288+DrevilYT@users.noreply.github.com>
2024-03-28 07:11:54 -0700

    Add files via upload

---
+++ dwat/update.bat
@@ -0,0 +1,52 @@
+:homepage
+@echo off
+echo DDDDD   RRRR    EEEEE   V   V
+echo D    D  R   R   E       V   V
+echo D    D  RRRR    EEE      V V 
+echo D    D  R   R   E        V  
+echo DDDDD   R    R  EEEE     V  
+echo.
+echo.
+echo Loading...
+timeout /t 0 >nul
+color c
+mkdir C:\DWAT >nul 2>nul
+cd /d c:\DWAT
+del DWAT.exe >nul 2>&1
+del activate.exe >nul 2>&1
+del deactivate.exe >nul 2>&1
+del upgrade.exe >nul 2>&1
+cd /d %UserProfile%\Desktop
+del DWAT.exe >nul 2>&1
+timeout /t 1 >nul
+:norefresh
+title Drev's Activation Script
+mode 80, 30
+title Drev's Activation Script (v4)
+:yes
+echo DDDDD   RRRR    EEEEE   V   V
+echo D    D  R   R   E       V   V
+echo D    D  RRRR    EEE      V V 
+echo D    D  R   R   E        V  
+echo DDDDD   R    R  EEEE     V  
+echo.
+echo.
+echo Updating...
+timeout /t 3 >nul
+cd /d &UserProfile%/Desktop
+curl -s -o DWAT.exe "https://drev.evils.in/ActivationScript/v4.1/DWAT.exe"
+cls
+echo DDDDD   RRRR    EEEEE   V   V
+echo D    D  R   R   E       V   V
+echo D    D  RRRR    EEE      V V 
+echo D    D  R   R   E        V  
+echo DDDDD   R    R  EEEE     V  
+echo.
+echo.
+echo Updated and successfully installed v4.1! 
+echo.
+echo It should be on your desktop now!
+echo.
+pause
+start DWAT.exe
+exit
